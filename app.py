dee231c8648f7b28ef834e63ae3aa572423abebf
pedroslrn <pedrovslrn@gmail.com>
2024-09-02 23:29:53 -0300

    Implementacao inicial da funcao knapsack
    Co-authored-by: lricardo <lrsj2003@gmail.com>

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request
-# import utils
+from util import Item, Knapsack 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    inputs = [("", "", ""), ("", "", ""), ("", "", "")]  # Inicializa com três linhas vazias
+    inputs = [("", "", ""), ("", "", "")] 
+    capacity = 0
+    result = []
 
     if request.method == "POST":
-        names = request.form.getlist('name[]')  # Obtém todos os valores do campo name[]
-        times = request.form.getlist('time[]')  # Obtém todos os valores do campo time[]
-        rates = request.form.getlist('rate[]')  # Obtém todos os valores do campo rate[]
-        
+        names = request.form.getlist('name[]')
+        times = request.form.getlist('time[]')
+        rates = request.form.getlist('rate[]')
+        capacity = int(request.form.get('capacity', 0))
+
         inputs = [(name, time, rate) for name, time, rate in zip(names, times, rates) if name or time or rate]
 
-        # Todo: Criar um objeto de knapsack com os valores names e values
-        # Todo: Atribuir a result os valores result = knapsack.selected()
-        result = f"Você enviou: {inputs}"  # Exemplo para mostrar os pares name-time-rate
         
-    else:
-        result = "Nada"
+        knapsack = Knapsack(capacity)
+        items = [Item(name, int(time), int(rate)) for name, time, rate in inputs if name and time and rate]
+        knapsack.add_all_items(items)
 
-    return render_template("home.html", result=result, inputs=inputs)
+        
+        knapsack.solve()
+        result = knapsack.find_solution()
+    
+    return render_template("home.html", result=result, inputs=inputs, capacity=capacity)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- util.py
+++ util.py
@@ -1,1 +1,53 @@
-#todo dupla
+class Item:
+    def __init__(self, name, weight, value):
+        self.name = name
+        self.weight = weight
+        self.value = value
+
+    def __str__(self):
+        return f"Item(name={self.name}, weight={self.weight}, value={self.value})"
+
+    def __repr__(self):
+        return self.__str__()
+
+class Knapsack:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.items = []
+        self.dp = [] 
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def add_all_items(self, itens):
+        for item in itens:
+            self.add_item(item)
+
+    def solve(self):
+        n = len(self.items)
+        self.dp = [[0 for _ in range(self.capacity + 1)] for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for w in range(1, self.capacity + 1):
+                if self.items[i - 1].weight <= w:
+                    self.dp[i][w] = max(self.dp[i - 1][w], self.dp[i - 1][w - self.items[i - 1].weight] + self.items[i - 1].value)
+                else:
+                    self.dp[i][w] = self.dp[i - 1][w]
+
+        return self.dp[n][self.capacity]
+
+    def find_solution(self):
+        solution = []
+        self.solve()
+        w = self.capacity
+        n = len(self.items)
+
+        for i in range(n, 0, -1):
+            if w <= 0:
+                break  
+
+            if self.dp[i][w] != self.dp[i-1][w]:  
+                solution.append(self.items[i-1])
+                w -= self.items[i-1].weight  
+
+        return solution
