8f41b3bef6845c471eb730a96c8b279b99524daf
Ricardo <lrsj2003@gmail.com>
2024-09-09 22:04:58 -0300

    final
    Co-authored-by: Pedro Victor Salerno <pedroslrn@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from flask import Flask, render_template, request
-from util import Item, Knapsack 
+from util import Item, Knapsack
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    inputs = [("", "", ""), ("", "", "")] 
+    inputs = [("", "", ""), ("", "", "")] # Inicializar a tabela com 2 linhas
     capacity = 0
     result = []
 
     if request.method == "POST":
+        capacity = int(request.form.get('capacity', 0))
+
         names = request.form.getlist('name[]')
         times = request.form.getlist('time[]')
         rates = request.form.getlist('rate[]')
-        capacity = int(request.form.get('capacity', 0))
-
         inputs = [(name, time, rate) for name, time, rate in zip(names, times, rates) if name or time or rate]
 
-        
+        # Criando a mochila
         knapsack = Knapsack(capacity)
-        items = [Item(name, int(time), int(rate)) for name, time, rate in inputs if name and time and rate]
-        knapsack.add_all_items(items)
 
-        
-        knapsack.solve()
+        # Cria um array de itens para os itens registrados e não nulos
+        items = [Item(name, int(time), int(rate)) for name, time, rate in inputs if name and time and rate]
+        # Add itens na mochila
+        knapsack.add_items(items)
+
+        # Encontra a solução
         result = knapsack.find_solution()
     
     return render_template("home.html", result=result, inputs=inputs, capacity=capacity)
--- util.py
+++ util.py
@@ -4,50 +4,46 @@ class Item:
         self.weight = weight
         self.value = value
 
-    def __str__(self):
-        return f"Item(name={self.name}, weight={self.weight}, value={self.value})"
-
-    def __repr__(self):
-        return self.__str__()
-
 class Knapsack:
     def __init__(self, capacity):
         self.capacity = capacity
         self.items = []
-        self.dp = [] 
+        self.memorization = [] 
 
-    def add_item(self, item):
-        self.items.append(item)
-
-    def add_all_items(self, itens):
+    def add_items(self, itens):
         for item in itens:
-            self.add_item(item)
+            self.items.append(item)
 
     def solve(self):
         n = len(self.items)
-        self.dp = [[0 for _ in range(self.capacity + 1)] for _ in range(n + 1)]
 
+        # Inicializando a matriz de memorizacao
+        self.memorization = [[0 for _ in range(self.capacity + 1)] for _ in range(n + 1)]
+
+        # Preenche iterativamente o "meio" da matriz
         for i in range(1, n + 1):
             for w in range(1, self.capacity + 1):
+                # Verifica se ainda cabe na mochila 
                 if self.items[i - 1].weight <= w:
-                    self.dp[i][w] = max(self.dp[i - 1][w], self.dp[i - 1][w - self.items[i - 1].weight] + self.items[i - 1].value)
+                    self.memorization[i][w] = max(self.memorization[i - 1][w], self.memorization[i - 1][w - self.items[i - 1].weight] + self.items[i - 1].value)
                 else:
-                    self.dp[i][w] = self.dp[i - 1][w]
+                    self.memorization[i][w] = self.memorization[i - 1][w]
 
-        return self.dp[n][self.capacity]
+        return self.memorization[n][self.capacity]
 
     def find_solution(self):
         solution = []
         self.solve()
         w = self.capacity
         n = len(self.items)
-
+        
         for i in range(n, 0, -1):
+            # Encheu a mochila
             if w <= 0:
                 break  
 
-            if self.dp[i][w] != self.dp[i-1][w]:  
-                solution.append(self.items[i-1])
-                w -= self.items[i-1].weight  
+            if self.memorization[i][w] != self.memorization[i-1][w]:  
+                solution.append(self.items[i-1]) # Adiciona na solucao o item levado
+                w -= self.items[i-1].weight 
 
         return solution
